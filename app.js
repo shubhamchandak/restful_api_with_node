01655fa858a39115a3abd4fb1315543924334cde
Shubham <shubhamschandak@rarecrew.com>
2019-02-17 16:58:06 +0530

    parse the body and handle CORS

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const app = express();
 
 // for logging requests
 const morgan = require('morgan');
+
+// for extracting incoming body
 const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    req.header('Access-Control-Allow-Origin', '*');
+    req.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
