8bc0f57bdad2abca50284b9fa6020f6fb5d5fdde
Shubham <shubhamschandak@rarecrew.com>
2019-02-23 15:12:42 +0530

    set mongodb and mongoose and productRoutes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 // for logging requests
 const morgan = require('morgan');
@@ -7,8 +8,12 @@ const morgan = require('morgan');
 // for extracting incoming body
 const bodyParser = require('body-parser');
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require('./api/routes/productsRoute');
+const orderRoutes = require('./api/routes/ordersRoute');
+
+mongoose.connect('mongodb://cherrybrooklyn:'+ process.env.MONGO_ATLAS_PASSWORD + '@cherrybrooklyn-shard-00-00-af8qu.mongodb.net:27017,cherrybrooklyn-shard-00-01-af8qu.mongodb.net:27017,cherrybrooklyn-shard-00-02-af8qu.mongodb.net:27017/test?ssl=true&replicaSet=CherryBrooklyn-shard-0&authSource=admin&retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
