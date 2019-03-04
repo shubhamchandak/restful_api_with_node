60ad79d26939e8ff1e052a7ccb62052922323b53
Shubham <shubhamschandak@rarecrew.com>
2019-03-04 21:59:38 +0530

    temporarily remove auth for development

--- api/routes/productsRoute.js
+++ api/routes/productsRoute.js
@@ -24,7 +24,7 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', checkAuth, (req, res, next) => {
+router.post('/', (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -75,7 +75,7 @@ router.get('/:productId', (req, res, next) => {
     });
 });
 
-router.patch('/:productId', checkAuth, (req, res, next) => {
+router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for(const ops of req.body){
@@ -96,7 +96,7 @@ router.patch('/:productId', checkAuth, (req, res, next) => {
     });
 });
 
-router.delete('/:productId', checkAuth, (req, res, next) => {
+router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
     .exec()
